d5526c17792b0cd77d6cea8f7ccaccb7d349a580
Anish Navada <navadaa1@bxscience.edu>
2019-10-13 09:51:38 -0400

    changed projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -10,6 +10,19 @@ class Projects extends React.Component{
 				<div className="project-row">
 					<div className="project card mb-3">
 					  <div className="row no-gutters">
+					      <img src={require('../images/music-recommendation.jpg')} className="project-image card-img-top" alt=""></img>
+					    <div className="project-desc col-md-8">
+					      <div className="card-body">
+					        <p className="project-name card-title">MusicRec (2019)</p>
+					        <p className="card-text">A Web-app made with React, MongoDB, Node JS, and Express that allows users to create and share music recommendation lists through auto-generated list-keys. All users with keys can edit/add to the music list.</p>
+					        <a href="https://music-rec-321.herokuapp.com" target="_blank"><p className="card-text">Click here to go to MusicRec!</p></a>
+					      </div>
+					    </div>
+					  </div>
+					</div>
+					
+					<div className="project card mb-3">
+					  <div className="row no-gutters">
 					      <img src={require('../images/player-compare.png')} className="project-image card-img-top" alt=""></img>
 					    <div className="project-desc col-md-8">
 					      <div className="card-body">
@@ -21,6 +34,22 @@ class Projects extends React.Component{
 					  </div>
 					</div>
 					
+				</div>
+				
+				<div className="project-row">
+					{/*<div className="project card mb-3">
+					  <div className="row no-gutters">
+					      <img src={require('../images/AnishNavada.jpg')} className="project-image card-img-top" alt=""></img>
+					    <div className="project-desc col-md-8">
+					      <div className="card-body">
+					        <p className="project-name card-title">Personal Website (2019)</p>
+					        <p className="card-text">This is the website you are on! It is intended to allow users to get to know my technical and career related experiences. This website is also made on React!</p>
+					        <a href="http://anishnavada.github.io/AnishNavada" target="_blank"><p className="card-text">Click here to go to Anish Navada!</p></a>
+					      </div>
+					    </div>
+					  </div>
+					</div>*/}
+					
 					<div className="project card mb-3">
 					  <div className="row no-gutters">
 					      <img src={require('../images/quantum.jpg')} className="project-image card-img-top" alt=""></img>
@@ -29,21 +58,6 @@ class Projects extends React.Component{
 					        <p className="project-name card-title">Quantum Key Distribution (2018)</p>
 					        <p className="card-text">An interactive Unity Program that allows users to follow the basics of how Quantum Key Distribution in Quantum Cryptography works. The program allows you to follow an example to explain a simple version of the concept.</p>
 					        <a href="http://anishnavada.github.io/QuantumKeyDistribution" target="_blank"><p className="card-text">Click here to go to Quantum Key Distribution!</p></a>
-					      </div>
-					    </div>
-					  </div>
-					</div>
-				</div>
-				
-				<div className="project-row">
-					<div className="project card mb-3">
-					  <div className="row no-gutters">
-					      <img src={require('../images/AnishNavada.jpg')} className="project-image card-img-top" alt=""></img>
-					    <div className="project-desc col-md-8">
-					      <div className="card-body">
-					        <p className="project-name card-title">Personal Website (2019)</p>
-					        <p className="card-text">This is the website you are on! It is intended to allow users to get to know my technical and career related experiences. This website is also made on React!</p>
-					        <a href="http://anishnavada.github.io/AnishNavada" target="_blank"><p className="card-text">Click here to go to Anish Navada!</p></a>
 					      </div>
 					    </div>
 					  </div>
